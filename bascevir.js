20db8ebb135977e031a6056a8b4538ff1302af08
Ahmet Can Aydemir <ahmetcanaydemir@users.noreply.github.com>
2020-02-13 12:49:45 +0300

    Refactoring styles

--- bascevir.js
+++ bascevir.js
@@ -1,87 +1,91 @@
 const linkCreator = {
-	cambridge: (sl, tl, word) => `https://dictionary.cambridge.org/dictionary/${sl}-${tl}/${word}`, 
+	cambridge: (sl, tl, word) => `https://dictionary.cambridge.org/dictionary/${sl}-${tl}/${word}`,
 	tureng: (sl, tl, word) => `https://tureng.com/en/${tl}-${sl}/${word}`
 }
 
 const getWord = (sl, tl, q) => {
-	q = q.replace(".", "").replace("?", "").replace(",", ",")
+  q = q.replace(/(\.|\?|,)/g, "");
 	return fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sl}&tl=${tl}&dt=t&q=${encodeURI(q)}`)
   	.then(res => res.ok ? res.json() : res.text());
 }
 
 const createPopup = (id, word, text, x, y, clientWidth) => {
-	let offsetX;
-	let r = (x > clientWidth / 2 ? 1 : -1);
-	if(r === 1){
-		offsetX = clientWidth - 350 - x;
-	}else{
-		offsetX = 350-x;
-	}
+	let offsetX = 350 - x;
+  let r = x > (clientWidth / 2) ? 1 : -1;
+  if (r === 1) {
+    offsetX = clientWidth - offsetX;
+  }
 
-	const popup = document.createElement("div");
-	popup.setAttribute("id", id)
-	popup.style.top = y + "px";
-	popup.style.left = x + "px";
-	popup.style.width = "0px";
-	popup.style.height = "0px";
-	popup.style.position = "absolute";
-	popup.style.zIndex = "5";
-	popup.style.userSelect = "none";
+  const popup = document.createElement("div");
+  popup.setAttribute("id", id);
+  popup.setAttribute("style",`
+    top:${y}px;
+    left:${y}px;
+    width:0px;
+    height:0px;
+    position:absolute;
+    z-index:5;
+    user-select:none;`);
 
-	const box = document.createElement("div");
-	box.setAttribute("class", "box");
-	box.style.display = "inline";
-	box.style.maxWidth = "300px";
-	box.style.width = "max-content";
-	box.style.position = "absolute";
-	box.style.marginTop = "-5px";
-	box.style.zIndex = "0";
+  const box = document.createElement("div");
+  box.setAttribute("class", "box");
+  box.setAttribute("style",`
+    display:inline;
+    max-width:300px;
+    width:max-content;
+    position:absolute;
+    margin-top:-5px;
+    z-index:0;`);
 
-	const link = document.createElement("a");
-	link.href = `https://translate.google.com/#view=home&op=translate&sl=en&tl=tr&text=${word}`;
-	link.target = "_blank";
-	link.style.outline = "0"
+  const link = document.createElement("a");
+  link.href = `https://translate.google.com/#view=home&op=translate&sl=en&tl=tr&text=${word}`;
+  link.target = "_blank";
+  link.style.outline = "0";
 
-	const content = document.createElement("span")
-	content.setAttribute("class", "onetap-content")
-	content.innerText = text;
-	content.style.borderRadius = "5px 0px 0px 5px";
-	content.style.padding = "10px 20px";
-	content.style.backgroundColor = "#333";
-	content.style.fontSize = "18px";
-	content.style.font = "Open Sans";
-	content.style.color = "#fff";
-	content.style.display = "inline-block";
+  const content = document.createElement("span");
+  content.innerText = text;
+  content.setAttribute("class", "onetap-content");
+  content.setAttribute("style",`
+    border-radius: 5px 0px 0px 5px;
+    padding: 10px 20px;
+    background-color: #333;
+    font-size: 18px;
+    font-family: Open Sans; 
+    color: #fff;
+    display: inline-block;`);
 
-	link.appendChild(content);
+  link.appendChild(content);
 
-	const removeButton = document.createElement("div");
-	removeButton.onclick = _ => removeHighlight(document.querySelectorAll(`span.${id}`))
-	removeButton.className = "highlight-remove-button";
-	removeButton.style.position = "absolute";
-	removeButton.style.height = "50%";
-	removeButton.style.width = "20px";
-	removeButton.style.top = "0";
-	removeButton.style.right = "-20px";
-	removeButton.style.textAlign = "center";
-	removeButton.style.color = "#fff";
-	removeButton.style.backgroundColor = "rgb(216, 76, 76)";
-	removeButton.style.borderRadius = "0px 5px 0px 0px";
-	removeButton.style.fontSize = "14px";
+  const removeButton = document.createElement("div");
+  removeButton.onclick = _ =>
+    removeHighlight(document.querySelectorAll(`span.${id}`));
+  removeButton.className = "highlight-remove-button";
+  removeButton.setAttribute("style",`
+    position: absolute;
+    height: 50%;
+    width: 20px;
+    top: 0;
+    right: -20px;
+    text-align: center;
+    color: #fff;
+    background-color: rgb(216, 76, 76);
+    border-radius: 0px 5px 0px 0px;
+    font-size: 14px;`);
 
-	const infoButton = document.createElement("div");
-	infoButton.onclick = _ => goDictionary(id, word) 
-	infoButton.className = "highlight-info-button";
-	infoButton.style.position = "absolute";
-	infoButton.style.height = "50%";
-	infoButton.style.width = "20px";
-	infoButton.style.top = "50%";
-	infoButton.style.right = "-20px";
-	infoButton.style.textAlign = "center";
-	infoButton.style.color = "#fff";
-	infoButton.style.backgroundColor = "rgb(0, 108, 255)";
-	infoButton.style.borderRadius = "0px 0px 5px 0px";
-	infoButton.style.fontSize = "14px";
+  const infoButton = document.createElement("div");
+  infoButton.onclick = _ => goDictionary(id, word);
+  infoButton.className = "highlight-info-button";
+  infoButton.setAttribute("style",`
+    position: absolute;
+    height: 50%;
+    width: 20px;
+    top: 50%;
+    right: -20px;
+    text-align: center;
+    color: #fff;
+    background-color: rgb(0, 108, 255);
+    border-radius: 0px 0px 5px 0px;
+    font-size: 14px;`);
 
 	box.appendChild(link)
 	box.appendChild(removeButton)
@@ -160,7 +164,7 @@ document.addEventListener("click",  event => {
 		.then(({permanentBoxesValue}) => {
 			const exclude_classnames = ["onetap-content", "highlight-info-button"];
 			if(!permanentBoxesValue && !Array.from(event.target.classList).some(_class => exclude_classnames.includes(_class))){
-				removeHighlight(document.querySelectorAll('span.onetap-highlight'))				
+				removeHighlight(document.querySelectorAll('span.onetap-highlight'))
 			}
 		})
 		return
